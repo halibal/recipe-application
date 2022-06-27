6512772ad29c2beb01a7fbeb43919a11fa3116fe
Halibal <halibal95@gmail.com>
2022-06-27 22:18:22 +0300

    routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Pages from './pages/Pages.jsx';
+import Category from './components/Category.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello</h1>
-			<Pages />
+			<BrowserRouter>
+				<Category />
+				<Pages />
+			</BrowserRouter>
 		</div>
 	);
 }
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -22,8 +22,7 @@ function Popular() {
         } else {
             const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
             const data = await api.json();
-
-            localStorage.setItem('popular', JSON.stringify(data.stringify));
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
             setPopular(data.recipes);
             console.log(data.recipes);
         }
